d681d972ba4fa14fcd5c52116ef48a11c47825f2
Tomasz <tomgaek@gmail.com>
2023-05-04 21:50:05 +0100

    add basic logic for comparing pictures

--- src/index.js
+++ src/index.js
@@ -10,17 +10,21 @@ const CARD_SIZE = 100;
 canvas.width = CANVAS_BORDER_WIDTH;
 canvas.height = CANVAS_BORDER_WIDTH;
 
+// temp arrays
+let clickedcard = [];
+let clickedimg = [];
+
 // Draw canvas borders
 ctx.beginPath();
 ctx.strokeRect(0, 0, CANVAS_BORDER_WIDTH, CANVAS_BORDER_WIDTH);
 
 // Lay all the cards
-const cardsLayed = new CardLayout(ctx, CARD_SIZE, CANVAS_BORDER_WIDTH);
+export const cardsLayed = new CardLayout(ctx, CARD_SIZE, CANVAS_BORDER_WIDTH);
 cardsLayed.layCards();
 
 // Randomly assign pictures to each card
-const test = new TossPictures();
-test.tossPics();
+export const shufflePics = new TossPictures();
+shufflePics.tossPics();
 
 // eventlistener that is able to locate wchich card is being clicked on
 canvas.addEventListener('click', (event) => {
@@ -31,9 +35,28 @@ canvas.addEventListener('click', (event) => {
     if (mouseX >= element.x && mouseX <= element.x + element.cardSize
         && mouseY >= element.y && mouseY <= element.y + element.cardSize) {
       console.log(`clicked: ${index}`);
+      console.log(shufflePics);
+      console.log(`randomArr: ${shufflePics.randomArr}`);
       // eslint-disable-next-line max-len
       cardsLayed.deleteCard(element.x, element.y); // dlaczego ta funkcja dziala tutaj ale nie poza eventlitener?
-      cardsLayed.replaceCard(index, test.picsArr[test.randomArr[index]]);
+      cardsLayed.replaceCard(index, shufflePics.picsArr[shufflePics.randomArr[index]]);
+      clickedcard.push(index);
+      clickedimg.push(shufflePics.picsArr[shufflePics.randomArr[index]]);
+      if (clickedimg.length >= 2) {
+        if (clickedimg[0] === clickedimg[1]) {
+          alert('congrats');
+          clickedcard = [];
+          clickedimg = [];
+        } else {
+          clickedcard.forEach((el) => {
+            cardsLayed.deleteCard(cardsLayed.imgArr[el].x, cardsLayed.imgArr[el].y);
+            cardsLayed.replaceCard(el, '../assets/images/backofcard.svg');
+
+            clickedcard = [];
+            clickedimg = [];
+          });
+        }
+      }
     }
   });
 });
